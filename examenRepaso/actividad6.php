6f692fad0e859f2de3a8d12c6565ceccde481b5c
Muhammad-Bin-Mahboob <muhammadbinm123@gmail.com>
2024-10-18 10:41:28 +0200

    Add files via upload

---
+++ examenRepaso/actividad6.php
@@ -0,0 +1,47 @@
+<?php 
+/**
+ * 
+ * @author Muhammad
+ * @version 1.0
+ */
+
+function addition(float $num1, float $num2):float  {
+    $result = $num1 - $num2;
+    return $result;
+}
+
+function subtration(float $num1, float $num2):float  {
+    $result = $num1 - $num2;
+    return $result;
+}
+
+function division(float $num1, float $num2):float  {
+    $result = $num1 / $num2;
+    return $result;
+}
+
+function multiplication(float $num1, float $num2):float {
+    $result = $num1 * $num2;
+    return $result;
+}
+
+function model(int $num1, int $num2) {
+    $result = ($num1 % $num2);
+    return $result;
+}
+
+function comparision(int $num1, int $num2):bool {
+    if($num1=$num2){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function pair(int $num1, int $num2):bool {
+     if($num1%2==0) {
+        return true;
+    } elseif ($num2%2==0) {
+        return false;
+    }
+}
