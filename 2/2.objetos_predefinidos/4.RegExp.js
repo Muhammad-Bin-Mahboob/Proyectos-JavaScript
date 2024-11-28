c02bda37f236a04a3be33ede410b29b82ff2a869
Muhammad Bin Mahboob <muhammadbinm123@gmail.com>
2024-11-28 18:18:49 +0100

    Delete 2 directory

--- 2/2.objetos_predefinidos/4.RegExp.js
+++
@@ -1,8 +0,0 @@
-var patrol = /serpi/i
-var patro2 = /serpi/
-var cad="Curso de JavaScript en el Serpis";
-console.log(patrol.test(cad)); //true 
-console.log(patro2.test(cad)); //false
-var res = cad.match(patrol);
-console.log(res.length); //1, una sola ocurrencia 
-console.log(res[0]); //Serpi
